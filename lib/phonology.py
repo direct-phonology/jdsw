8d60ac1fcfab4d9c28e974749e90c9d144280fb9
Nick Budak <budak@stanford.edu>
2022-05-10 15:16:05 -0400

    Update Reconstruction to work with Gian's table

--- lib/phonology.py
+++ lib/phonology.py
@@ -14,58 +14,58 @@ class Reconstruction:
         self.table = table.drop_duplicates()
 
     def initial_for(self, char: str) -> str:
-        readings = self.table[self.table["zi"] == char]
+        readings = self.table[self.table["char"] == char]
         if len(readings) == 0:
             raise NoReadingError
-        initials = readings["MCInitial"].unique()
+        initials = readings["initial"].unique()
         if len(initials) > 1:
             raise MultipleReadingsError
         return initials[0]
 
-    def final_for(self, char: str) -> str:
-        readings = self.table[self.table["zi"] == char]
+    def rime_for(self, char: str) -> str:
+        readings = self.table[self.table["char"] == char]
         if len(readings) == 0:
             raise NoReadingError
-        finals = readings["MCfinal"].unique()
-        if len(finals) > 1:
+        rimes = readings["rime"].unique()
+        if len(rimes) > 1:
             raise MultipleReadingsError
-        return finals[0]
+        return rimes[0]
 
     def reading_for(self, char: str) -> str:
-        readings = self.table[self.table["zi"] == char]
+        readings = self.table[self.table["char"] == char]
         if len(readings) == 0:
             raise NoReadingError
         if len(readings) > 1:
             raise MultipleReadingsError
-        return readings["MC"].iloc[0]
+        return readings["reading"].iloc[0]
 
     def readings_for(self, char: str) -> list[str]:
-        return self.table[self.table["zi"] == char]["MC"].unique().tolist()
+        return self.table[self.table["char"] == char]["reading"].unique().tolist()
 
     def fanqie_reading_for(self, initial: str, final: str) -> str:
-        initial_reading = self.initial_for(initial).rstrip("-")
-        final_reading = self.final_for(final).lstrip("-")
+        initial_reading = self.initial_for(initial)
+        rime_reading = self.rime_for(final)
 
         # special cases
         # y + j = y
         # yh + j = yh
-        if (initial_reading[-1] == "y" and final_reading[0] == "j") or (
-            initial_reading[-2:] == "yh" and final_reading[0] == "j"
+        if (initial_reading[-1] == "y" and rime_reading[0] == "j") or (
+            initial_reading[-2:] == "yh" and rime_reading[0] == "j"
         ):
-            return f"{initial_reading}{final_reading[1:]}"
+            return f"{initial_reading}{rime_reading[1:]}"
 
         # j + j = j
         # j + w = w
         # j + i = i
         if (
-            (initial_reading[-1] == "j" and final_reading[0] == "j")
-            or (initial_reading[-1] == "j" and final_reading[0] == "w")
-            or (initial_reading[-1] == "j" and final_reading[0] == "i")
+            (initial_reading[-1] == "j" and rime_reading[0] == "j")
+            or (initial_reading[-1] == "j" and rime_reading[0] == "w")
+            or (initial_reading[-1] == "j" and rime_reading[0] == "i")
         ):
-            return f"{initial_reading[:-1]}{final_reading}"
+            return f"{initial_reading[:-1]}{rime_reading}"
 
         # base case
-        return f"{initial_reading}{final_reading}"
+        return f"{initial_reading}{rime_reading}"
 
     def is_valid_reading(self, char: str, reading: str) -> bool:
         return reading in self.readings_for(char)
