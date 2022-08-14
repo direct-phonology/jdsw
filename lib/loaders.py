3aa4853e0db63a0e10b8dc3e50798cee9931958d
Nick Budak <budak@stanford.edu>
2022-08-14 14:00:27 -0700

    Refactor Kanripo txt and xml loaders

--- lib/loaders.py
+++ lib/loaders.py
@@ -1,5 +1,6 @@
 import re
 from typing import Iterator
+from xml.etree import ElementTree as ET
 
 from pathlib import Path
 from torch.utils.data import Dataset
@@ -7,6 +8,9 @@ from collections import defaultdict
 
 
 class KanripoDataset(Dataset):
+    krid = re.compile(r"(KR|CH)\d\w\d{4}")
+
+class KanripoLocalDataset(KanripoDataset):
     """
     Dataset of documents from the Kanseki repository stored on a filesystem.
 
@@ -14,22 +18,20 @@ class KanripoDataset(Dataset):
     Kanripo identifiers (e.g. KR1a0001). Files are loaded at access time when
     iterating, or can be accessed directly by keying on document ID.
 
-    Joins documents that were split across multiple files: KR1a0001_001.txt,
-    KR1a0001_002.txt, and so on will be joined into a single document with ID
-    KR1a0001.
-
-    Limits to txt files by default unless another `ext` is specified.
+    If merge = True, joins documents that were split across multiple files: 
+    KR1a0001_001, KR1a0001_002, and so on will be joined into a single 
+    document with ID KR1a0001.
     """
+    ext: str
 
-    krid = re.compile(r"(KR|CH)\d\w\d{4}")
-
-    def __init__(self, path: Path | str, ext: str = "txt") -> None:
+    def __init__(self, path: Path | str, merge: bool = False) -> None:
         # allow passing a string or Path object
         self.path = path if isinstance(path, Path) else Path(path)
+        self.merge = merge
 
         # build a map of document IDs to files in that document
         self.docs = defaultdict(list)
-        for file in self.path.glob(f"**/*{ext}"):
+        for file in self.path.glob(f"**/*{self.ext}"):
             docid = self.krid.search(file.name)
             if docid:
                 self.docs[docid.group(0)].append(file)
@@ -40,15 +42,38 @@ class KanripoDataset(Dataset):
     def __contains__(self, docid: str) -> bool:
         return docid in self.docs
 
-    def __getitem__(self, docid: str) -> str:
-        # read all files in the document in order and join them into a string
-        return "".join(
-            (f.read_text(encoding="utf-8") for f in sorted(self.docs[docid]))
-        )
+    def __getitem__(self, docid: str) -> list[Path]:
+        return self.docs[docid]
+
+class KanripoTxtDataset(KanripoLocalDataset):
+    ext = "txt"
 
     def __iter__(self) -> Iterator[str]:
-        # for each document ordered by id, yield text of that document
-        return (self[docid] for docid in sorted(self.docs))
+        for docid in sorted(self.docs):
+            if self.merge:
+                yield "".join((f.read_text(encoding="utf-8") for f in sorted(self[docid])))
+            else:
+                for f in sorted(self[docid]):
+                    yield f.read_text(encoding="utf-8")
+
+class KanripoXmlDataset(KanripoLocalDataset):
+    ext = "xml"
+    ns = {
+        "tei": "http://www.tei-c.org/ns/1.0",
+        "xml": "http://www.w3.org/XML/1998/namespace",
+    }
+
+    def __iter__(self) -> Iterator[ET.Element]:
+        for docid in sorted(self.docs):
+            contents = "".join((f.read_text(encoding="utf-8") for f in sorted(self[docid])))
+            tei = ET.fromstring(contents)
+            if self.merge:
+                for doc in tei.findall("./tei:text/tei:body", self.ns):
+                    yield doc
+            else:
+                for juan in tei.findall("./tei:text/tei:body/tei:div", self.ns):
+                    yield juan
+
 
 
 # TODO: KanripoRemoteDataset?
