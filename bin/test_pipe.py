a7d250df5c2e319b6d159d830392b576a43cbe5f
Nick Budak <budak@stanford.edu>
2022-08-14 14:01:00 -0700

    Update test pipe script

--- bin/test_pipe.py
+++ bin/test_pipe.py
@@ -5,18 +5,19 @@ from fastcore.transform import Pipeline
 
 from lib.transforms import (
     KanripoUnicode,
-    KanripoTeiXml,
     remove_comments,
     remove_page_breaks,
     remove_pilcrows,
     remove_whitespace,
+    remove_xml_tags,
 )
 
-from lib.loaders import KanripoDataset
+from lib.loaders import KanripoTxtDataset, KanripoXmlDataset
 
 def main() -> None:
-    zhengwen_txt = KanripoDataset("txt/zhengwen")
-    zhengwen_xml = KanripoDataset("txt/zhengwen", ext="xml")
+    zhengwen_txt = KanripoTxtDataset("txt/zhengwen")
+    zhengwen_xml = KanripoXmlDataset("txt/zhengwen")
+    jdsw_txt = KanripoTxtDataset("txt/jdsw")
 
     txt_pipe = Pipeline(funcs=[
         KanripoUnicode(),
@@ -25,14 +26,18 @@ def main() -> None:
         remove_pilcrows,
         remove_whitespace,
     ])
-    xml_pipe = Pipeline(funcs=[KanripoTeiXml(), *txt_pipe])
+    xml_pipe = Pipeline(funcs=[
+        remove_xml_tags,
+        *txt_pipe,
+    ])
 
     zhengwen = chain(
         (txt_pipe(t) for t in zhengwen_txt),
         (xml_pipe(t) for t in zhengwen_xml),
     )
+    jdsw = (txt_pipe(t) for t in jdsw_txt)
 
-    print([t[:15] for t in zhengwen])
+    print([t[:15] for t in jdsw])
 
 
 if __name__ == "__main__":
