84101f2dd877cee041b3c9c71c671a52715ac3ed
Nick Budak <budak@stanford.edu>
2022-08-18 12:37:29 -0700

    Update test script

--- bin/test_pipe.py
+++ bin/test_pipe.py
@@ -2,42 +2,50 @@ from itertools import chain
 
 import typer
 from fastcore.transform import Pipeline
-
+from lib.loaders import KanripoTxtDataset, KanripoXmlDataset
 from lib.transforms import (
     KanripoUnicode,
-    remove_comments,
-    remove_page_breaks,
-    remove_pilcrows,
-    remove_whitespace,
-    remove_xml_tags,
+    RemoveComments,
+    RemovePageBreaks,
+    RemoveChars,
+    RemoveWhitespace,
+    HealAnnotations,
+    ExtractAnnotations
 )
+from lib.documents import Alignment
 
-from lib.loaders import KanripoTxtDataset, KanripoXmlDataset
 
 def main() -> None:
     zhengwen_txt = KanripoTxtDataset("txt/zhengwen")
     zhengwen_xml = KanripoXmlDataset("txt/zhengwen")
     jdsw_txt = KanripoTxtDataset("txt/jdsw")
 
-    txt_pipe = Pipeline(funcs=[
-        KanripoUnicode(),
-        remove_comments,
-        remove_page_breaks,
-        remove_pilcrows,
-        remove_whitespace,
-    ])
-    xml_pipe = Pipeline(funcs=[
-        remove_xml_tags,
-        *txt_pipe,
-    ])
+    text_pipe = Pipeline(
+        funcs=[
+            KanripoUnicode,
+            RemoveComments,
+            RemovePageBreaks,
+            RemoveChars("0123456789.．¶*"),
+            RemoveWhitespace,
+        ]
+    )
+    anno_pipe = Pipeline(
+        funcs=[
+            *text_pipe,
+            HealAnnotations,
+            ExtractAnnotations
+        ]
+    )
 
     zhengwen = chain(
-        (txt_pipe(t) for t in zhengwen_txt),
-        (xml_pipe(t) for t in zhengwen_xml),
+        (text_pipe(doc) for doc in zhengwen_txt),
+        (text_pipe(doc) for doc in zhengwen_xml),
     )
-    jdsw = (txt_pipe(t) for t in jdsw_txt)
+    jdsw = (anno_pipe(t) for t in jdsw_txt)
 
-    print([t[:15] for t in jdsw])
+    out = list(jdsw)
+    print(out)
+
 
 
 if __name__ == "__main__":
