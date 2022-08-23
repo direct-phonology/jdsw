6f19b73b0177e514e946c5e446fcb841c7f64810
Nick Budak <budak@stanford.edu>
2022-08-22 21:34:50 -0700

    Update test pipe

--- bin/test_pipe.py
+++ bin/test_pipe.py
@@ -1,7 +1,10 @@
 from itertools import chain
+import csv
 
 import typer
 from fastcore.transform import Pipeline
+from lib.alignment import Alignment
+from lib.documents import KanripoDoc
 from lib.loaders import KanripoTxtDataset, KanripoXmlDataset
 from lib.transforms import (
     KanripoUnicode,
@@ -10,9 +13,8 @@ from lib.transforms import (
     RemoveChars,
     RemoveWhitespace,
     HealAnnotations,
-    ExtractAnnotations
+    ExtractAnnotations,
 )
-from lib.documents import Alignment
 
 
 def main() -> None:
@@ -31,21 +33,43 @@ def main() -> None:
     )
     anno_pipe = Pipeline(
         funcs=[
-            *text_pipe,
             HealAnnotations,
-            ExtractAnnotations
+            ExtractAnnotations,
         ]
     )
 
-    zhengwen = chain(
-        (text_pipe(doc) for doc in zhengwen_txt),
-        (text_pipe(doc) for doc in zhengwen_xml),
+    zhengwen_docs = map(text_pipe, chain(zhengwen_txt, zhengwen_xml))
+    jdsw_docs = map(text_pipe, jdsw_txt)
+    all_docs = {doc.id: doc for doc in chain(zhengwen_docs, jdsw_docs)}
+    align_map: dict[KanripoDoc, KanripoDoc] = {}
+
+    # laozi: combine all zhengwen juan into one doc and align with jdsw
+    zw_laozi_text = [doc for doc in all_docs.values() if doc.id.startswith("KR5c0057")]
+    zhengwen_laozi = KanripoDoc(
+        id="KR5c0057",
+        text="".join([doc.text for doc in sorted(zw_laozi_text)]),
     )
-    jdsw = (anno_pipe(t) for t in jdsw_txt)
+    align_map[zhengwen_laozi] = all_docs["KR1g0003_025"]
 
-    out = list(jdsw)
-    print(out)
+    # everything else: use juan.csv to align; join jdsw juan into one doc if multiple
+    reader = csv.DictReader(open("data/juan.csv", encoding="utf-8"))
+    for row in reader:
+        jdsw_juan = [all_docs[doc_id] for doc_id in row["jdsw_id"].split(",")]
+        align_map[all_docs[row["zhengwen_id"]]] = KanripoDoc(
+            id=row["jdsw_id"],
+            text="".join([doc.text for doc in jdsw_juan]),
+        )
 
+    # run the annotation pipe on the joined versions so annotation indices are correct
+    for zhengwen_doc, jdsw_doc in align_map.items():
+        align_map[zhengwen_doc] = anno_pipe(jdsw_doc)
+
+    # align the zhengwen and jdsw versions
+    alignments = [Alignment(zhengwen, jdsw) for zhengwen, jdsw in align_map.items()]
+    for alignment in alignments[:5]:
+        print(alignment)
+
+    # transfer the annotations using the aligment
 
 
 if __name__ == "__main__":
