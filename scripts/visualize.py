253ad249e4b4302f6c39f722930b9447a9eee0d5
Nick Budak <budak@stanford.edu>
2022-11-05 16:37:07 -0700

    Add NER pattern visualization script

---
+++ scripts/visualize.py
@@ -0,0 +1,51 @@
+"""Various streamlit-powered visualizations of the _Jingdian Shiwen_ data."""
+
+import streamlit as st
+import pandas as pd
+import spacy
+from pathlib import Path
+import srsly
+import altair as alt
+
+@st.cache
+def load_data():
+    """Load the data."""
+    return pd.read_csv("assets/annotations.csv")
+
+st.title("NER pattern matching")
+
+pattern_files = Path("assets").glob("*ner_patterns.jsonl")
+pattern_file = st.selectbox("Select an NER pattern file", pattern_files)
+
+if pattern_file:
+    patterns = list(srsly.read_jsonl(pattern_file))
+else:
+    patterns = []
+
+nlp = spacy.blank("och")
+ruler = nlp.add_pipe("entity_ruler")
+ruler.add_patterns(patterns)
+
+dataset = load_data()
+rows = []
+for annotation in dataset["annotation"]:
+    doc = nlp(annotation)
+    for ent in doc.ents:
+        rows.append({
+            "text": ent.text,
+            "label": ent.label_,
+            "annotation": annotation,
+            "title": dataset[dataset["annotation"] == annotation]["title"].values[0]
+        })
+hits = pd.DataFrame(rows, columns=["text", "label", "annotation", "title"])
+
+st.write("## Found patterns")
+st.write("### Total: {}".format(len(pd.unique(hits["text"]))))
+top_n = st.slider("Show top: ", 1, 50, 15)
+top = pd.DataFrame(hits["text"].value_counts()[:top_n].rename_axis("pattern").reset_index(name="count"))
+st.write(alt.Chart(top, width=700).mark_bar().encode(x=alt.X("pattern", sort=None), y="count"))
+
+missing = set([pattern["pattern"] for pattern in patterns]) - set(hits["text"])
+st.write("## Missing patterns")
+st.write("### Total: {}".format(len(missing)))
+st.write(", ".join(sorted(missing)))
